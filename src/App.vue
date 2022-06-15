a1dce0eca487138087427b701853164b2abd015e
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-06-15 11:42:42 -0300

    feat: setup vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,5 @@
 
 <template>
   <h1>Hello World</h1>
+  <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'virtual:windi.css'
+import { router } from './routes'
 
 const app = createApp(App)
 
+app.use(router)
 app.mount('#app')
