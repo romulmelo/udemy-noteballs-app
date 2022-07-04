76311c894a71747ef6c23adfe0d3eded4f11b4fe
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-04 19:41:45 -0300

    chore: add icons and auto-import-components library

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 
-import vue from '@vitejs/plugin-vue'
-import windicss from 'vite-plugin-windicss'
+import Vue from '@vitejs/plugin-vue'
+
+import WindiCSS from 'vite-plugin-windicss'
+
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
-  plugins: [vue(), windicss()],
+  plugins: [
+    Vue(),
+    Components({
+      resolvers: [IconsResolver({ prefix: false, enabledCollections: ['ph'] })]
+    }),
+    Icons({
+      autoInstall: true
+    }),
+    WindiCSS()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
