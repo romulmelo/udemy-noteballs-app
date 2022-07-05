aba278c872c80f17174e4eb46825e000ec2114eb
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-05 12:19:05 -0300

    chore: remove unused plugin

--- windi.config.js
+++ windi.config.js
@@ -30,7 +30,6 @@ export default defineConfig({
   },
   plugins: [
     require('windicss/plugin/filters'),
-    require('windicss/plugin/forms'),
     require('windicss/plugin/aspect-ratio'),
     require('windicss/plugin/line-clamp'),
     require('windicss/plugin/typography')
