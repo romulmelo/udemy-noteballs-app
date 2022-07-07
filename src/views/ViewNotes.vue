856816b9d89aadfd9739e4c8ab72c0e05f77df8a
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-06 21:02:36 -0300

    feat: ensure fallback content is displayed if there are no notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -4,14 +4,7 @@ import { useFocus } from '@vueuse/core'
 
 const input = ref()
 const showModal = ref(false)
-const notes = ref([
-  {
-    id: 1,
-    title: 'Title',
-    content: 'Some description',
-    toDelete: false
-  }
-])
+const notes = ref([])
 const newNote = reactive({
   title: '',
   content: ''
@@ -48,7 +41,24 @@ useFocus(input, { initialValue: true })
   </AppHeader>
   <main class="bg-zinc-50">
     <div class="flex flex-col p-10">
-      <div class="flex text-zinc-800 gap-4 items-center">
+      <div v-if="notes.length === 0" class="flex flex-col gap-6 items-center">
+        <img
+          src="src/static/img/no-results-found.svg"
+          alt="Thow empty notebooks"
+          loading="lazy"
+          width="240"
+          height="240"
+        />
+        <div class="flex flex-col text-center gap-1 items-center">
+          <p class="font-medium text-zinc-400/80 lg:text-lg">
+            Oops. Looks like there are no notes around here :(
+          </p>
+          <p class="font-medium text-zinc-400/80 lg:text-lg">
+            Click on Add a new note button and register everything here.
+          </p>
+        </div>
+      </div>
+      <div v-else class="flex text-zinc-800 gap-4 items-center">
         <span class="font-semibold text-[14px] uppercase">All notes</span>
         <span class="rounded font-semibold border-2 border-zinc-800 px-2">
           3
